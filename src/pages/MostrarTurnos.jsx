b5cc54270ff18763e24a5c28734e358edb1d8239
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-27 08:15:41 -0300

    refactoring mostrarTurnos

--- src/pages/MostrarTurnos.jsx
+++ src/pages/MostrarTurnos.jsx
@@ -4,17 +4,26 @@ import { today } from "../helpers/today";
 import { splitStr } from "../helpers/splitStr";
 
 const MostrarTurnos = () => {
-    const [turnos, setTurnos] = useState([])
-    useEffect(() => {
-        ipcConnect.filterData('get-turno-filter', 'fecha', today).then(result => {
-            setTurnos(result.sort((a, b) => a.hora.localeCompare(b.hora)))
+    
+    const [turnos, setTurnos] = useState([]);
 
-        })
+    useEffect(() => {
+        const fetchTurnos = async () => {
+            try {
+                const result = await ipcConnect.filterData('get-turno-filter', 'fecha', today);
+                const sortedTurnos = result.sort((a, b) => a.hora.localeCompare(b.hora));
+                setTurnos(sortedTurnos);
+            } catch (error) {
+                console.error('Failed to fetch turnos:', error);
+            }
+        };
+
+        fetchTurnos();
     }, []);
     return (
-      <div className="fixed inset-0 z-50 bg-slate-200 ">
-        <h2 className="text-center mt-5 font-bold text-xl">Turnos {today.split('-').reverse().join('/')}</h2>
-        <div className="mt-2 shadow-sm border rounded-lg overflow-x-auto">
+        <div className="fixed inset-0 z-50 bg-slate-200 ">
+            <h2 className="text-center mt-5 font-bold text-xl">Turnos {today.split('-').reverse().join('/')}</h2>
+            <div className="mt-2 shadow-sm border rounded-lg overflow-x-auto">
                 <table className="w-full table-auto  text-left">
                     <thead className="bg-blue-200 text-gray-600 font-medium border-b">
                         <tr>
@@ -31,7 +40,7 @@ const MostrarTurnos = () => {
                                 .sort((a, b) => a.hora.localeCompare(b.hora))
                                 .map((item, idx) => (
                                     <tr key={idx} className={idx % 2 === 0 ? `bg-slate-100 text-pink-600` : ''}>
-                                        <td className="px-4 py-1 whitespace-nowrap font-medium">                                            
+                                        <td className="px-4 py-1 whitespace-nowrap font-medium">
                                             {splitStr(item.paciente?.nombre)} - {splitStr(item.paciente?.apellido)}
                                         </td>
                                         <td className="py-1 whitespace-nowrap text-center">
@@ -48,9 +57,9 @@ const MostrarTurnos = () => {
                     </tbody>
                 </table>
             </div>
-      </div>
+        </div>
     );
-  };
-  
+};
+
 
 export default MostrarTurnos;
